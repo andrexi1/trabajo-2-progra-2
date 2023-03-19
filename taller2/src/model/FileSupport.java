f35207610f29344493de17f5905874af897fe43a
andres <andresbarrera427@gmail.com>
2023-03-18 23:07:36 -0500

    clase FileSupport

---
+++ taller2/src/model/FileSupport.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.io.File;
+
+/**
+ * 
+ * @author Andres barrera, javier lopez y Diego Patiño
+ *
+ */
+public class FileSupport {
+	private File fileIn;
+
+	/**
+	 * 
+	 * @param fileIn
+	 */
+	public FileSupport(File fileIn) {
+		super();
+		this.fileIn = fileIn;
+	}
+
+	/**
+	 * 
+	 * @return
+	 */
+	public File getFileIn() {
+		return fileIn;
+	}
+
+	/**
+	 * 
+	 * @param fileIn
+	 */
+	public void setFileIn(File fileIn) {
+		this.fileIn = fileIn;
+	}
+
+	/**
+	 * 
+	 */
+	@Override
+	public String toString() {
+		return "FileSupport [fileIn=" + fileIn + "]";
+	}
+
+}
