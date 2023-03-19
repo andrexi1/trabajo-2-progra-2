4e04a9f9fef8aa48e557fb3a3fc71a2176071cd8
javierlink22@gmail.com <javierlink22@gmail.com>
2023-03-19 08:41:35 -0500

    Novedades

--- taller2/src/com/edu/uptc/prgII/Taller2/view/ViewScanner.java
+++ taller2/src/com/edu/uptc/prgII/Taller2/view/ViewScanner.java
@@ -1,5 +1,43 @@
-package com.edu.uptc.prgII.Taller2.view;
+package com.edu.uptc.prgII.activityNo2.view;
 
+import java.util.Scanner;
+
+/**
+ * 
+ * @author Andres barrera, javier lopez y Diego Patiño
+ *
+ */
 public class ViewScanner {
-    
+	private Scanner console;
+
+	/**
+	 * 
+	 */
+	public ViewScanner() {
+		console = new Scanner(System.in);
+
+	}
+
+	/**
+	 * 
+	 * @param message
+	 * @return
+	 */
+	public String readString(String message) {
+		showMessage(message);
+		return console.nextLine();
+
+	}
+
+	/**
+	 * 
+	 * @param message
+	 */
+	public void showMessage(String message) {
+		System.out.println(message);
+	}
+	
+	public void breaker() {
+		console.close();
+	}
 }
